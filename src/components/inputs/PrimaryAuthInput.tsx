559338a4c52839274f8e00ee765c84c0987bb5b4
othdr <otmax.vft@gmail.com>
2024-02-26 21:44:43 +0100

    input styling

--- src/components/inputs/PrimaryAuthInput.tsx
+++ src/components/inputs/PrimaryAuthInput.tsx
@@ -20,18 +20,18 @@ const PrimaryAuthInput: React.FC<Props> = ({
     const { t } = useTranslation();
 
     return (
-        <div className='relative w-fit flex-col items-start pb-2'>
+        <div className='relative w-full flex-col items-start pb-2'>
 
             {label &&
                 <label
                     htmlFor={name}
-                    className='block text-start pb-1 text-sm md:text-base text-baseGray font-normal'
+                    className='px-2 block text-start pb-1 text-sm md:text-base text-baseGray font-normal'
                 >
                     {t(label)}
                 </label>
             }
             <input
-                className='p-2 md:p-3 text-baseGray text-sm md:text-base placeholder:text-baseGray/50 rounded-full border border-baseGray bg-gradient-to-br from-darkBlue to-darkPurple'
+                className='p-2 md:p-3 w-full text-baseGray text-sm md:text-base placeholder:text-baseGray/50 rounded-full border border-baseGray bg-gradient-to-br from-darkBlue to-darkPurple outline-none focus:shadow-md focus:border-primaryBlue focus:shadow-purple-600/50 duration-200'
                 type={inputType}
                 placeholder={t(placeHolder)}
                 onBlur={formik.handleBlur}
--- src/scenes/auth/SignIn.tsx
+++ src/scenes/auth/SignIn.tsx
@@ -32,7 +32,10 @@ const SignIn: React.FC = () => {
             <img className='w-1/2' src={bg} alt="" />
 
             <div className="w-1/2 flex flex-col items-center justify-center">
-                <form action="">
+                <form
+                    className='w-full md:w-[400px] space-y-2 md:space-y-4'
+                    action=""
+                >
                     <PrimaryAuthInput
                         name="email"
                         label="email"
